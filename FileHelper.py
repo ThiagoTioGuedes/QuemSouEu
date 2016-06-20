19f0684ae6112f5b19e6da27dd2bbad68075f449
unknown <thiago.tioguedes@gmail.com>
2016-06-19 23:12:50 -0300

    initial commit. Only basic functionalities are working

---
+++ FileHelper.py
@@ -0,0 +1,30 @@
+__author__ = 'Thiago'
+import xml.etree.cElementTree as ET
+import os
+from xml.dom.minidom import parse
+
+def lista_bibliotecas():
+    return os.listdir('Bibliotecas');
+
+
+def criar_pasta_bibliotecas():
+    if not os.path.exists('Bibliotecas'):
+        os.makedirs('Bibliotecas')
+
+
+def abrir_arquivo_biblioteca(nome_arquivo):
+    # Aqui abrimos o arquivo XML usando o minidom parser
+    domtree = parse('Bibliotecas\\'+nome_arquivo+".xml")
+    collection = domtree.documentElement
+
+    # Pega todos os itens que foram cadastrados
+    return collection.getElementsByTagName("Item")
+
+
+def criar_arquivo(nome_arquivo, lista_itens=[]):
+    root = ET.Element(nome_arquivo)
+
+    for item in lista_itens:
+        ET.SubElement(root, "Item").text = item
+
+    ET.ElementTree(root).write('Bibliotecas\\'+ nome_arquivo + ".xml")
---
+++ main.py
@@ -0,0 +1,65 @@
+import FileHelper
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, ListProperty
+
+# biblioteca que foi selecionada pelo usuario, necessario para a sua edicao
+biblioteca_selecionada = ''
+
+class EditaBiblioteca(Screen):
+    nome_item = ObjectProperty(None)
+    lista_itens = ListProperty([])
+    lista_view = ObjectProperty(None)
+    biblioteca_label = ObjectProperty(None)
+
+    def on_enter(self, *args):
+        self.biblioteca_label.text = biblioteca_selecionada
+
+    def criar_item(self):
+        self.lista_itens.append(self.nome_item.text)
+
+    def remover_item(self):
+        self.lista_itens.remove(self.lista_view.adapter.selection[0].text)
+
+
+
+
+
+class ListaBiblioteca(Screen):
+    nome_biblioteca = ObjectProperty(None)
+    lista_bibliotecas = ListProperty([])
+    lista_view = ObjectProperty(None)
+
+    def on_enter(self, *args):
+        self.lista_bibliotecas = FileHelper.lista_bibliotecas();
+
+    def criar_biblioteca(self):
+        self.lista_bibliotecas.append(self.nome_biblioteca.text)
+        FileHelper.criar_arquivo(self.nome_biblioteca.text, self.lista_bibliotecas)
+
+    def editar_biblioteca(self):
+        global biblioteca_selecionada
+        biblioteca_selecionada = self.lista_view.adapter.selection[0].text
+        # o pai eh sm, o screenManager
+        self.parent.current = 'editaBiblioteca'
+
+
+class MenuPrincipal(Screen):
+    pass
+
+
+class QuemSouEuApp(App):
+
+    def build(self):
+        FileHelper.criar_pasta_bibliotecas()
+        # Create the screen manager
+        sm = ScreenManager()
+        sm.add_widget(MenuPrincipal(name='menu'))
+        sm.add_widget(ListaBiblioteca(name='listaBiblioteca'))
+        sm.add_widget(EditaBiblioteca(name='editaBiblioteca'))
+
+        return sm
+
+
+if __name__ == '__main__':
+    QuemSouEuApp().run()
