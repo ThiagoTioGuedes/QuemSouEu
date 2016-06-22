cdbbe06ba59a860ad385b11b62e868300ce3cc82
TioGuedes <thiago.tioguedes@gmail.com>
2016-06-22 13:45:43 +0000

    Expands the dropbox API examples

--- NetHelper.py
+++ NetHelper.py
@@ -3,12 +3,21 @@
 # de identificacao ja foi gerado
 
 import dropbox
+from random import randint 
 
-client = dropbox.client.DropboxClient('rv7QP0_5jm4AAAAAAAAA47qM2pvueMuIRRKWe3P3isZz0yUr9Oaeoju0lnDXpWpE')
+token_validacao = 'rv7QP0_5jm4AAAAAAAAA47qM2pvueMuIRRKWe3P3isZz0yUr9Oaeoju0lnDXpWpE'
+
+# Esse e o codigo token de validacao, importante mante-lo em seguranca
+client = dropbox.client.DropboxClient(token_validacao)
 print 'linked account: ', client.account_info()
 
 f = open('working-draft.txt', 'w+')
-response = client.put_file('/magnum-opus.txt', f)
+f.write(str(randint(1,10000)));
+f.close()
+f = open('working-draft.txt', 'r')
+
+
+response = client.put_file('/magnum-opus.txt', f, overwrite=True)
 print 'uploaded: ', response
 
 folder_metadata = client.metadata('/')
