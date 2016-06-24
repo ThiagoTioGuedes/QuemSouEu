d91eeb71aacdff7e20cca5c583143e8dacd1077e
ThiagoTioGuedes <thiago.tioguedes@gmail.com>
2016-06-24 13:44:52 +0000

    Adds basic sound support

---
+++ AssetsHelper.py
@@ -0,0 +1,19 @@
+import os
+from kivy.core.audio import SoundLoader
+
+class SoundManager:
+    def __init__(self):
+        self.sound = carregar_som('sound.wav')
+        
+    def toca_som(self):
+        if self.sound:
+            self.sound.play()    
+
+
+def carregar_som(nome_som):
+    return SoundLoader.load('Assets'+os.sep+nome_som)
+    
+
+
+
+    
